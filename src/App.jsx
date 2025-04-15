a0cb894d54773e71c6ab950079517853c7e5c65b
Zamil <zamilaziz92@gmail.com>
2025-04-15 16:42:09 +0800

    innitial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>This is a simple React application.</p>
+        </div>
+    );
+};
+
+export default App;
