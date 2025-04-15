f568fe6d3f3b33b29ad352c62cb21898ee636248
Zamil <zamilaziz92@gmail.com>
2025-04-15 17:02:15 +0800

    init tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1 className='text-white-50 flex-center'>Hello</h1>
             <p>This is a simple React application.</p>
         </div>
     );
