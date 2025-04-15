504b8e9738293093d16352033f1264b1ba85da5f
Zamil <zamilaziz92@gmail.com>
2025-04-15 19:44:47 +0800

    add word animation to hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import Hero from './sections/Hero';
 
 const App = () => {
     return (
-        <div>
-            <h1 className='text-white-50 flex-center h-20'>Hello</h1>
-            <p>This is a simple React application.</p>
-        </div>
+        <>
+            <Hero />
+        </>
     );
 };
 
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,41 @@
+import { words } from '../constants';
+const Hero = () => {
+    return (
+        <section id='hero' className='relative overflow-hidden'>
+            <div className='absolute top-0 left-0 z-10'>
+                <img src='/images/bg.png' alt='background' />
+            </div>
+            <div className='hero-layout'>
+                {/* LEFT: HERO CONTENT */}
+                <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+                    <div className='flex flex-col gap-7'>
+                        <div className='hero-text'>
+                            <h1>
+                                Shaping
+                                <span className='slide'>
+                                    <span className='wrapper'>
+                                        {words.map(word => (
+                                            <span key={word.text} className='flex items-center md:gap-3 gap-3 pb-2'>
+                                                <img
+                                                    src={word.imgPath}
+                                                    alt={word.text}
+                                                    className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'
+                                                />
+                                                <span>{word.text}</span>
+                                            </span>
+                                        ))}
+                                    </span>
+                                </span>
+                            </h1>
+                            <h1>into Real Project</h1>
+                            <h1>that Deliver Results</h1>
+                        </div>
+                    </div>
+                </header>
+                {/* RIGHT: 3D MODEL */}
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
