a2621ccd0f5ed45749a81edf768f57ae8a9a188a
Zamil <zamilaziz92@gmail.com>
2025-04-21 17:37:39 +0800

    add text loading animation on Hero with GSAP

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,18 @@
 import Button from '../components/Button';
 import { words } from '../constants';
 import HeroExperience from '../components/HeroModels/HeroExperience';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.fromTo(
+            '.hero-text h1',
+            { y: 50, opacity: 0 },
+            { y: 0, opacity: 1, duration: 1, stagger: 0.2, ease: 'power2.inOut' }
+        );
+    });
+
     return (
         <section id='hero' className='relative overflow-hidden'>
             <div className='absolute top-0 left-0 z-10'>
