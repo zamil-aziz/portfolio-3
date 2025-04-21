c1004e2c0d4175d09fcc21558e24826dee63854c
Zamil <zamilaziz92@gmail.com>
2025-04-21 18:31:29 +0800

    change button's logic

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,21 @@
 const Button = ({ text, className, id }) => {
     return (
-        <a className={`${className ?? ''} cta-wrapper`}>
+        <a
+            onClick={e => {
+                e.preventDefault();
+                const target = document.querySelector('conter');
+
+                if (target) {
+                    const offset = window.innerHeight * 0.15;
+                    const top = target.getBoundingClientRect().top + window.scrollY - offset;
+                    window.scrollTo({
+                        top: top,
+                        behavior: 'smooth',
+                    });
+                }
+            }}
+            className={`${className ?? ''} cta-wrapper`}
+        >
             <div className='cta-button group'>
                 <div className='bg-circle' />
                 <p className='text'>{text}</p>
