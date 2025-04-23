081dfddeaebfab694f05942260ccc88ccba34fac
Zamil <zamilaziz92@gmail.com>
2025-04-23 13:53:48 +0800

    add ref's to be used for animation

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,10 +1,17 @@
+import { useRef } from 'react';
+
 const ShowcaseSection = () => {
+    const sectionRef = useRef(null);
+    const project1Ref = useRef(null);
+    const project2Ref = useRef(null);
+    const project3Ref = useRef(null);
+
     return (
-        <div id='work' className='app-showcase'>
+        <section id='work' ref={sectionRef} className='app-showcase'>
             <div className='w-full'>
                 <div className='showcaselayout'>
                     {/* LEFT */}
-                    <div className='first-project-wrapper'>
+                    <div className='first-project-wrapper' ref={project1Ref}>
                         <div className='image-wrapper'>
                             <img src='/images/project1.png' alt='Project 1' />
                         </div>
@@ -15,7 +22,7 @@ const ShowcaseSection = () => {
                     </div>
                     {/* RIGHT */}
                     <div className='project-list-wrapper overflow-hidden'>
-                        <div className='project'>
+                        <div className='project' ref={project2Ref}>
                             <div className='image-wrapper bg-[#ffefdb]'>
                                 <img src='/images/project2.png' alt='Project 2' />
                             </div>
@@ -23,7 +30,7 @@ const ShowcaseSection = () => {
                                 <h2>Project Title</h2>
                             </div>
                         </div>
-                        <div className='project'>
+                        <div className='project' ref={project3Ref}>
                             <div className='image-wrapper bg-[#ffe7eb]'>
                                 <img src='/images/project3.png' alt='Project 3' />
                             </div>
@@ -34,7 +41,7 @@ const ShowcaseSection = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
