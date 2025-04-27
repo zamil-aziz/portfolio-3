5f5ed3de3613b48e31599407543dc5f3ba06d70b
Zamil <zamilaziz92@gmail.com>
2025-04-27 16:21:11 +0800

    add navbar base component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NavBar from './components/NavBar';
 import Hero from './sections/Hero';
 import ShowcaseSection from './sections/ShowcaseSection';
 
 const App = () => {
     return (
         <>
+            <NavBar />
             <Hero />
             <ShowcaseSection />
         </>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <header className='navbar'>
+            <div className='inner'>
+                <a className='logo' href='#hero'>
+                    Zamil.dev
+                </a>
+                <nav className='desktop'>asd</nav>
+            </div>
+        </header>
+    );
+};
+
+export default NavBar;
