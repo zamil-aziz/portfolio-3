bc96839f1be8bad0ef4da6dc9914475ebd7994db
Zamil <zamilaziz92@gmail.com>
2025-04-27 16:40:43 +0800

    add navbar contents

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,5 @@
+import { navLinks } from '../constants/index.js';
+
 const NavBar = () => {
     return (
         <header className='navbar'>
@@ -5,7 +7,25 @@ const NavBar = () => {
                 <a className='logo' href='#hero'>
                     Zamil.dev
                 </a>
-                <nav className='desktop'>asd</nav>
+
+                <nav className='desktop'>
+                    <ul>
+                        {navLinks.map(({ link, name }) => (
+                            <li key={name} className='group'>
+                                <a href={link}>
+                                    <span>{name}</span>
+                                    <span className='underline' />
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+
+                <a href='#contact' className='contact-btn group'>
+                    <div className='inner'>
+                        <span>Contact Me</span>
+                    </div>
+                </a>
             </div>
         </header>
     );
