05c36f8a550f663bbd3edae6cfa5540774c7ffbd
Zamil <zamilaziz92@gmail.com>
2025-05-01 16:27:31 +0800

    add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LogoSection from './sections/LogoSection';
 import FeatureCards from './sections/FeatureCards';
 import ExperienceSection from './sections/ExperienceSection';
 import TechStack from './sections/TechStack';
+import Contact from './sections/Contact';
 
 const App = () => {
     return (
@@ -16,6 +17,7 @@ const App = () => {
             <FeatureCards />
             <ExperienceSection />
             <TechStack />
+            <Contact />
         </>
     );
 };
