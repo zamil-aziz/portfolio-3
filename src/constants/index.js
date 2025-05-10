0633a8055b0abf5a92b6d0ecc34770f6eb9ce0a2
Zamil <zamilaziz92@gmail.com>
2025-05-10 13:39:09 +0800

    add changes to hero section

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,9 +25,9 @@ const words = [
 ];
 
 const counterItems = [
-    { value: 15, suffix: '+', label: 'Years of Experience' },
-    { value: 200, suffix: '+', label: 'Satisfied Clients' },
-    { value: 108, suffix: '+', label: 'Completed Projects' },
+    { value: 4, suffix: '+', label: 'Years of Experience' },
+    { value: 5, suffix: '+', label: 'Satisfied Clients' },
+    { value: 10, suffix: '+', label: 'Completed Projects' },
     { value: 90, suffix: '%', label: 'Client Retention Rate' },
 ];
 
